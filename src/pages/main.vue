148b3b207fd94892989808f152e0646737448c6d
RapatNG <84861045+RapatNG@users.noreply.github.com>
2021-05-27 16:58:28 +0700

    Initial commit

---
+++ src/pages/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <manufacturer v-slot="data">
+      <devices :manufacturerId="data.factId"></devices>
+    </manufacturer>
+  </div>
+</template>
+
+<script>
+import manufacturer from "@/components/manufacturer.vue";
+import devices from "@/components/devices.vue";
+export default {
+  components: {
+    Manufacturer: manufacturer,
+    Devices: devices,
+  },
+};
+</script>
